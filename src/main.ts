2997cc3fbca00cb45870a90b6f21247c4f2ae529
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-09-17 20:29:11 +0200

    add Role access

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
+import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new LoggerInterceptor());
   app.setGlobalPrefix('api');
   app.use(cookieParser());
 
   await app.enableShutdownHooks();
-  await app.listen(3000);
+  await app.listen(configService.get('port'));
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
 import {
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
 
 @Controller('users')
 export class UsersController {
@@ -24,5 +28,17 @@ export class UsersController {
       throw new NotFoundException(`User with id ${id} not found`);
     }
     return user;
+  }
+
+  @Delete('/:id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  public async deleteUser(@Param('id', new ParseUUIDPipe()) id: User['id']) {
+    const user = await this.userService.getUser(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    await this.userService.delete(id);
+    return { message: `User ${user.email} deleted` };
   }
 }
